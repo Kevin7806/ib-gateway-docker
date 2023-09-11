b5d82cbc2a507f38d72c6a66ecd0c99d0f5793a0
Kelvin Lu <lu.f.kelvin@gmail.com>
2023-09-10 19:46:13 -0500

    Enforce docker secret in container

--- stable/DockerBuild.sh
+++ stable/DockerBuild.sh
@@ -1,30 +1,33 @@
 #!/bin/sh
 
-docker build -t ib_gateway-image .
-docker image tag ib_gateway-image:latest ib_gateway-image:stable
+docker build -t ib_gateway-image:stable .
 
 docker run -d   --name iTraderBot.IB.Gateway \
                 --restart always \
-                --network iTradeBotNetwork \
                 --env-file .env \
                 -p 4001:4001 \
                 -p 4002:4002 \
                 -p 5900:5900 \
                 ib_gateway-image:stable
 
-# ##############################################
+# ###########################################################################
 # The following is using docker service
-# we have issue with --network iTradeBotNetwork
-# ##############################################
+#
+#   Use following command to create secrets:
+#   =========================================================================
+#   printf "***********" | docker secret create iStrategy-IBKR-TWS_USERID -
+#   printf "***********" | docker secret create iStrategy-IBKR-TWS_PASSWORD -
+#   =========================================================================
+#
+# ###########################################################################
 docker service rm IBGateway-Service     # remove existing services
 docker service create \
             --name IBGateway-Service \
-            --network iTradeBotNetwork \
+            --network iConnect \
             --reserve-memory=2GB \
             --replicas 1 \
-            --secret iTraderBot_UID \
-            --secret iTraderBot_PWD \
-            --secret iTraderBot_VNC \
+            --secret iStrategy-IBKR-TWS_USERID \
+            --secret iStrategy-IBKR-TWS_PASSWORD \
             --env-file .env \
             --publish published=4001,target=4001,mode=host \
             --publish published=4002,target=4002,mode=host \
